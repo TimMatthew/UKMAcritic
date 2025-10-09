738344d183a2209f03562dde8c6d6e4d36ac7421
Tymofii Matiichyk <t.mattw.0000@gmail.com>
2025-10-09 22:33:55 +0300

    Intial project setup with configured PSQL connection

---
+++ src/main/java/org/spring/ukmacritic/UkmacriticApplication.java
@@ -0,0 +1,13 @@
+package org.spring.ukmacritic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkmacriticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkmacriticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/ukmacritic/UkmacriticApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.ukmacritic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkmacriticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
