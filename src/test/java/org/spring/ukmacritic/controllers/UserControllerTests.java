be060151105864d55857847a72bd3bd19c2090c9
siishsusu <byvladyslava@gmail.com>
2025-10-11 21:35:13 +0300

    deleted imports that are not used

--- src/test/java/org/spring/ukmacritic/controllers/UserControllerTests.java
+++ src/test/java/org/spring/ukmacritic/controllers/UserControllerTests.java
@@ -1,25 +1,19 @@
 package org.spring.ukmacritic.controllers;
 
 import org.junit.jupiter.api.Test;
-import org.spring.ukmacritic.dto.UserTestDto;
 import org.spring.ukmacritic.entities.User;
 import org.spring.ukmacritic.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.client.match.ContentRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
