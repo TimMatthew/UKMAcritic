7e2147e680e453783e33414c0bfa32c9d4e1666e
siishsusu <byvladyslava@gmail.com>
2025-10-11 21:40:10 +0300

    changed mock tests file name

--- src/test/java/org/spring/ukmacritic/controllers/UserControllerMockTests.java
+++ src/test/java/org/spring/ukmacritic/controllers/UserControllerMockTests.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class UserControllerTest {
+class UserControllerMockTests {
     @Mock
     private UserService userService;
 
